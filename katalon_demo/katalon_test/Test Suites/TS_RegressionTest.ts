b84aaca258544dcd4aafe48f7816851f265d4d0b
ze wang <zefiber@gmail.com>
2019-05-04 14:24:31 -0400

    Add test suite

---
+++ katalon_demo/katalon_test/Test Suites/TS_RegressionTest.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_RegressionTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>38d68816-a2a3-426b-bd49-3644a9ff46d5</testSuiteGuid>
+   <testCaseLink>
+      <guid>4fd9afa8-8d3d-458b-8fe3-fe536fdd9dab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/buyItem</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
